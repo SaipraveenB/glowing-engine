7ee1e0925689935977168b480d88ab0182331b20
JS Suhas <j.s.suhas@live.in>
2016-09-04 19:14:07 +0530

    Added comments.

--- MemoryInstructions.h
+++ MemoryInstructions.h
@@ -7,15 +7,21 @@
 
 #include "Instruction.h"
 
+// Load Instruction (LD)
 class LoadInstruction : public Instruction {
  public:
+  // Register containing base address.
   unsigned short base_register;
+
+  // Register containing offset.
   unsigned short register_offset;
 
+  // Register that will contain the output of LD instruction.
   unsigned short output_register;
 
   void execute(RegisterFile<unsigned short> *rf, Memory<char> *mem);
 
+  // Register Load Instruction with Instruction Factory.
   class LoadFactory : public Factory {
    public:
     void registerName(map<string, Instruction::Factory *> *directory, vector<Instruction::Factory *> *vec);
@@ -28,15 +34,22 @@ class LoadInstruction : public Instruction {
   };
 };
 
+// Store Instruction (SD)
 class StoreInstruction : public Instruction {
  public:
+
+  // Register containing base address.
   unsigned short base_register;
+
+  // Register containing offset from base address.
   unsigned short register_offset;
 
+  // Register containing the source of data.
   unsigned short input_register;
 
   void execute(RegisterFile<unsigned short> *rf, Memory<char> *mem);
 
+  // Register Store Instruction with Instruction Factory.
   class StoreFactory : public Factory {
    public:
     void registerName(map<string, Instruction::Factory *> *directory, vector<Instruction::Factory *> *vec);
